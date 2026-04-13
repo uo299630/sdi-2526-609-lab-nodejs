be981f1a1f2516316d4ea8c8b98c92f16616299d
uo299630 <luisj@LuisPC>
2026-04-13 16:59:30 +0200

    SDI-2526-609-11.6-SW Servicio Memoria.

---
+++ hello-world-server.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let app = express();
+let os = require('os');
+let port = 3000;
+app.get('/memory', function (req, res) {
+    setTimeout(function () {
+        console.log(os.freemem());
+        var freeMemory = os.freemem() / 1000000;
+        res.status(200);
+        res.json({
+            memory: freeMemory
+        });
+    }, 10000);
+});
+app.listen(port, function () {
+    console.log("Servidor listo " + port);
+});
+
