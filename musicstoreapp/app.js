bcc501a93228ea82fe39d764c14ca01fbb4b04f5
uo299630 <luisj@LuisPC>
2026-04-13 16:47:27 +0200

    SDI-2526-609-11.3-SW Cliente JQuery + AJAX

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -14,6 +14,14 @@ var indexRouter = require('./routes/index');
 var app = express();
 app.set('jwt', jwt);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    next();
+});
+
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
---
+++ musicstoreapp/routes/userTokenRouter.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const express = require('express');
+const userTokenRouter = express.Router();
+userTokenRouter.use(function (req, res, next) {
+    console.log("userTokenRouter");
+    let token = req.headers['token'] || req.body.token || req.query.token;
+    if (token != null) {
+        // verificar el token
+        jwt.verify(token, 'secreto', {}, function (err, infoToken) {
+            if (err || (Date.now() / 1000 - infoToken.time) > 240) {
+                res.status(403).json({ //Forbidden
+                    authorized: false,
+                    error: 'Token inválido o caducado'
+                });
+            } else {
+                // dejamos correr la petición
+                res.user = infoToken.user;
+                next();
+            }
+        });
+    } else {
+        res.status(403).json({
+            authorized: false,
+            error: 'No hay Token'
+        });
+    }
+});
+module.exports = userTokenRouter;
